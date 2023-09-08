0a9e67d2e1f618e7a8b28ad43069795b30554445
cnx-luizalberton <luiz.alberton@conexasaude.com.br>
2023-09-08 18:09:26 -0300

    initial user controller created

--- challenge/src/main/java/bluecode/tech/challenge/controllers/UserController.java
+++ challenge/src/main/java/bluecode/tech/challenge/controllers/UserController.java
@@ -1,4 +1,35 @@
 package bluecode.tech.challenge.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/users")
 public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping
+    public ResponseEntity<Object> createUser(@RequestBody User user) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(userService.createUser(user));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Object> getUser(@PathVariable Long id) {
+        return ResponseEntity.status(HttpStatus.OK).body(userService.getUser(id));
+    }
+
+    @PostMapping("/{id}")
+    public ResponseEntity<Object> updateUser(@PathVariable Long id, @RequestBody User user) {
+        return ResponseEntity.status(HttpStatus.OK).body(userService.updateUser(id));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> updateUser(@PathVariable Long id) {
+        return ResponseEntity.status(HttpStatus.OK).body(userService.deleteUser(id));
+    }
+
 }
